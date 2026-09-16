9dc91fb5ff51579aabd1183a35e08e36d4070143
jjimenezg331-dot <jjimenezg331@gmail.com>
2026-09-16 09:52:52 -0600

    Crear bloque para resistencia interna

--- main.ts
+++ main.ts
@@ -1,1 +1,20 @@
-pins.setPull(DigitalPin.P0, PinPullMode.PullDown)
+namespace pins {
+    /**
+     * Establece la resistencia interna de un pin digital.
+     * @param pin pin digital que se configurará
+     * @param modo tipo de resistencia interna
+     */
+    //% blockId=establecer_resistencia_interna
+    //% block="establecer resistencia interna en pin $pin a $modo"
+    //% pin.shadow=digital_pin_shadow
+    //% pin.defl=DigitalPin.P0
+    //% modo.defl=PinPullMode.PullDown
+    //% advanced=true
+    //% weight=14
+    export function establecerResistenciaInterna(
+        pin: DigitalPin,
+        modo: PinPullMode
+    ): void {
+        pins.setPull(pin, modo)
+    }
+}
